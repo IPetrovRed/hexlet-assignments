2184cd0d753df114161960eeedded9bf52ff3b99
@hexlet/cli <support@hexlet.io>
2024-04-29 20:04:21 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -1,14 +1,17 @@
 package exercise;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 // BEGIN
 public class App {
 
     public static List<String> buildApartmentsList(List<Home> apartments, int n) {
         return apartments.stream()
-                .sorted()
+                .sorted((a, b) -> (int) (a.getArea() - b.getArea())) // Сортировать по площади
                 .limit(n)
                 .map(Home::toString)
                 .collect(Collectors.toList());
